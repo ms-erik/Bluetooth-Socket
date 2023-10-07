8011d3330d2e48096e9286667c9c84f6b70cda1b
ms-erik <erikmelges@usp.br>
2023-10-07 13:02:21 -0300

    Solved conection issues between server and client

--- client.c
+++ client.c
@@ -5,56 +5,68 @@
 #include <unistd.h>
 
 int main() {
-  struct sockaddr_rc addr = {0};
-  int s, status;
-  char dest[18] = "B0:A4:60:5F:C1:60";
+    struct sockaddr_rc addr = {0};
+    int s, status;
+    char dest[18] = "B0:A4:60:5F:C1:60";
 
-  // alocate a socket
-  s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    // Allocate a socket
+    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
-  // set the connection parameters
-  addr.rc_family = AF_BLUETOOTH;
-  addr.rc_channel = (uint8_t)1;
-  str2ba(dest, &addr.rc_bdaddr);
+    // Set the connection parameters
+    addr.rc_family = AF_BLUETOOTH;
+    addr.rc_channel = (uint8_t)1;
+    str2ba(dest, &addr.rc_bdaddr);
 
-  // connect to server
-  status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+    // Connect to the server
+    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
 
-  // send message
-  if (status == 0) {
-      printf("Welcome to Hangman\n");
-      char guess[2];
-      while(1){
-          //receive info from server 
-          char message[1024];
-          memset(message, 0, sizeof(message)-1);
-          status = recv(s, message, sizeof(message)-1,0);
-          if(status <=0){
-              printf("Server disconnected");
-              break; 
-          }
-          //Display game info from server 
-          printf("%s", message);
+    // Send message
+    if (status == 0) {
+        printf("Welcome to Hangman\n");
+        char guess[2];
+        while (1) {
+            // Receive info from server
+            char message[1024];
+            memset(message, 0, sizeof(message));
 
-          //if gameOver, exit loop
-          if(strstr(message, "Game Over") != NULL){
-              break;
-          }
+            // Receive data from the server
+            status = recv(s, message, sizeof(message) - 1, 0);
+            message[status] = '\0';
 
-          //if the game is stille going, prompt the user to guess a letter
-          printf("Enter a letter guess: ");
-          fgets(guess, sizeof(guess), stdin);
+            if (status <= 0) {
+                printf("Server disconnected\n");
+                break;
+            }
 
-          //send the letter guess to the server 
-          status = write(s, guess, strlen(guess));
-          if(status < 0){
-              perror("Write error");
-              break; 
-          }
-      }
-  } else {
-      perror("Connection error");
-  }
-  close(s);
-  return 0;
+            // Display game info from server
+            printf("%s", message);
+
+            // Check if the game is over
+            if (strstr(message, "Game Over") != NULL) {
+                break;
+            }
+
+            // If the game is still going, prompt the user to guess a letter
+            fgets(guess, sizeof(guess), stdin);
+            guess[strcspn(guess, "\n")] = '\0';
+
+            int c;
+            while((c = getchar()) != '\n' && c != EOF) {}
+
+            // Send the letter guess to the server
+            send(s, guess, strlen(guess), 0);
+            if (status < 0) {
+                perror("Write error");
+                break;
+            }
+        }
+    } else {
+        perror("Connection error");
+    }
+
+    // Close the socket
+    close(s);
+
+    return 0;
 }
+
--- server.c
+++ server.c
@@ -6,107 +6,108 @@
 #include <time.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_TRIES 6
 
 char *wordList[] = {"invadir", "servidor", "bahia"};
 
-char* getRandomWord(){
-    srand(time(NULL));
-    int randomIndex = rand() % (sizeof(wordList)/ sizeof(wordList[0]));
+char *getRandomWord() {
+    int randomIndex = rand() % (sizeof(wordList) / sizeof(wordList[0]));
     return wordList[randomIndex];
 }
 
 int main() {
-  struct sockaddr_rc loc_addr = {0}, rem_addr = {0};
-  char buf[1024] = {0};
-  int server_socket, client, bytes_read;
-  socklen_t opt = sizeof(rem_addr);
+    struct sockaddr_rc loc_addr = {0}, rem_addr = {0};
+    char buf[1024] = {0};
+    int server_socket, client, bytes_read = 0; // Initialize bytes_read
+    socklen_t opt = sizeof(rem_addr);
 
-  // alocate socket
-  server_socket = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    // Allocate socket
+    server_socket = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
-  // bind socket to port 1 of the first avaiable
-  // local  bluetooth adapter
-  loc_addr.rc_family = AF_BLUETOOTH;
-  loc_addr.rc_bdaddr = *BDADDR_ANY;
-  loc_addr.rc_channel = (uint8_t)1;
-  if (bind(server_socket, (struct sockaddr *)&loc_addr, sizeof(loc_addr))) {
-    perror("Bind falied\n");
-    return 1;
-  }
-
-  // put socket into listening mode
-  listen(server_socket, 1);
-
-  // acept one connection
-  client = accept(server_socket, (struct sockaddr *)&rem_addr, &opt);
-
-  //seed the random number generator
-  srand(time(NULL));
-
-  char* wordToGuess = getRandomWord();
-  int wordLength = strlen(wordToGuess);
-
-  char* currentWordState = (char*)malloc(wordLength+1);
-  memset(currentWordState, '_', wordLength);
-  currentWordState[wordLength] = '\0';
-
-  int tries =0;
-  bool gameOver = false;
-
-  /*
-  ba2str(&rem_addr.rc_bdaddr, buf);
-  fprintf(stderr, "accepted connection from %s\n", buf);
-  memset(buf, 0, sizeof(buf));
-
-  // read data from client
-  while ((bytes_read = read(client, buf, sizeof(buf))) > 0) {
-    printf("received %s\n", buf);
-    memset(buf, 0, sizeof(buf));
-  }
-  */
-  while(!gameOver){
-    //send the current state of the word and the number of tries left
-    sprintf(buf, "Word: %s\nTries Left: %d\n", currentWordState, MAX_TRIES - tries);
-    send(client, buf, strlen(buf), 0);
-    if(bytes_read <=0){
-        printf("Client disconnected\n");
-        break;
+    // Bind socket to port 1 of the first available local Bluetooth adapter
+    loc_addr.rc_family = AF_BLUETOOTH;
+    loc_addr.rc_bdaddr = *BDADDR_ANY;
+    loc_addr.rc_channel = (uint8_t)1;
+    if (bind(server_socket, (struct sockaddr *)&loc_addr, sizeof(loc_addr))) {
+        perror("Bind failed\n");
+        return 1;
     }
-    buf[bytes_read] = '\0';
-    
-    //check if the guessed letters are in the word
-    char guess = buf[0];
+
+    // Put socket into listening mode
+    listen(server_socket, 1);
+
+    // Accept one connection
+    client = accept(server_socket, (struct sockaddr *)&rem_addr, &opt);
+
+    // Seed the random number generator
+    srand(time(NULL));
+
+    char *wordToGuess = getRandomWord();
+    int wordLength = strlen(wordToGuess);
+
+    char *currentWordState = (char *)malloc(wordLength + 1);
+    memset(currentWordState, '_', wordLength);
+    currentWordState[wordLength] = '\0';
+
+    int tries = 0;
+    bool gameOver = false;
     bool correctGuess = false;
-    for(int i=0; i< wordLength; i++){
-        if(wordToGuess[i] == guess && currentWordState[i] == '_'){
-            currentWordState[i] == guess;
-            correctGuess = true;
+
+    while (!gameOver) {
+        // Send the current state of the word to the client
+        sprintf(buf, "Word: %s\nTries Left: %d\nEnter a letter to guess: ", currentWordState, MAX_TRIES-tries);
+        send(client, buf, strlen(buf), 0);
+        // Receive a letter guess from the client
+        memset(buf,0,sizeof(buf));
+        bytes_read = recv(client, buf, sizeof(buf)-1, 0);
+        buf[bytes_read] = '\0';
+
+        // Check if the client disconnected
+        if (bytes_read <= 0) {
+            printf("Client disconnected\n");
+            break;
+        }
+
+        // Extract the guessed letter
+        char guess = buf[0];
+        char aux = '_';
+        correctGuess = false;
+
+        // Check if the guessed letters are in the word
+        for (int i = 0; i < wordLength; i++) {
+            if (wordToGuess[i] == guess && currentWordState[i] == aux) {
+                currentWordState[i] = guess;
+                correctGuess = true;
+            }
+        }
+
+        if (!correctGuess) {
+            tries++;
+        }
+
+        // Check if the game is over
+        if (strcmp(currentWordState, wordToGuess) == 0 || tries >= MAX_TRIES) {
+            gameOver = true;
         }
     }
-    if(!correctGuess){
-        tries++;
-    }
-    //check if the game is gameOver 
-    if(strcmp(currentWordState, wordToGuess)==0 || tries >= MAX_TRIES){
-        gameOver = true;
-    }
-  }
 
-  //send the final game result to the client
-  if(strcmp(currentWordState, wordToGuess) ==0){
-      strcpy(buf, "Congratulations, you won!\n");
-  }else{
-      strcpy(buf, "Game Over! the word was: ");
-      strcat(buf, wordToGuess);
-      strcat(buf, "\n");
-  }
+    // Send the final game result to the client
+    if (strcmp(currentWordState, wordToGuess) == 0) {
+        strcpy(buf, "Congratulations, you won!\n");
+    } else {
+        strcpy(buf, "Game Over! The word was: ");
+        strcat(buf, wordToGuess);
+        strcat(buf, "\n");
+    }
 
-  send(client, buf, strlen(buf), 0);
-  // close connection
-  close(client);
-  close(server_socket);
-  free(currentWordState);
-  return 0;
+    send(client, buf, strlen(buf), 0);
+
+    // Close connection
+    close(client);
+    close(server_socket);
+    free(currentWordState);
+    return 0;
 }
+
